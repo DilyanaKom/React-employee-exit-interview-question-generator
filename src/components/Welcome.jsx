71415c5e0999e044857dbd2e044c9f2349bc500a
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-07 14:40:18 +0300

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -13,28 +13,28 @@ export default function WelcomeScreen() {
                     <p className="font-medium text-gray-800 mb-1">Your responses are:</p>
                     <ul className="ml-5 text-gray-600 space-y-1">
                         <li className="flex items-start">
-                            <svg class="h-[1lh] w-5.5 shrink-0" viewBox="0 0 22 22" fill="none" stroke-linecap="square">
-                                <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
-                                <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
-                                <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+                            <svg className="h-[1lh] w-5.5 shrink-0" viewBox="0 0 22 22" fill="none" strokeLinecap="square">
+                                <circle cx="11" cy="11" r="11" className="fill-sky-400/25" />
+                                <circle cx="11" cy="11" r="10.5" className="stroke-sky-400/25" />
+                                <path d="M8 11.5L10.5 14L14 8" className="stroke-sky-800 dark:stroke-sky-300" />
                             </svg>
                             <p className="ml-3">Confidential</p>
 
                         </li>
                         <li className="flex items-start">
-                            <svg class="h-[1lh] w-5.5 shrink-0" viewBox="0 0 22 22" fill="none" stroke-linecap="square">
-                                <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
-                                <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
-                                <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+                            <svg className="h-[1lh] w-5.5 shrink-0" viewBox="0 0 22 22" fill="none" strokeLinecap="square">
+                                <circle cx="11" cy="11" r="11" className="fill-sky-400/25" />
+                                <circle cx="11" cy="11" r="10.5" className="stroke-sky-400/25" />
+                                <path d="M8 11.5L10.5 14L14 8" className="stroke-sky-800 dark:stroke-sky-300" />
                             </svg>
                             <p className="ml-3">Anonymous in reports</p>
 
                         </li>
                         <li className="flex items-start">
-                        <svg class="h-[1lh] w-5.5 shrink-0" viewBox="0 0 22 22" fill="none" stroke-linecap="square">
-                                <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
-                                <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
-                                <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+                        <svg className="h-[1lh] w-5.5 shrink-0" viewBox="0 0 22 22" fill="none" strokeLinecap="square">
+                                <circle cx="11" cy="11" r="11" className="fill-sky-400/25" />
+                                <circle cx="11" cy="11" r="10.5" className="stroke-sky-400/25" />
+                                <path d="M8 11.5L10.5 14L14 8" className="stroke-sky-800 dark:stroke-sky-300" />
                             </svg>
                             <p className="ml-3">  Used to improve workplace </p>
                           
