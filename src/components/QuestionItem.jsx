34dded22a87632abecbaabac6ada1e7a7838701b
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-08 14:51:52 +0300

    Create QuestionItem.jsx

---
+++ src/components/QuestionItem.jsx
@@ -0,0 +1,10 @@
+export default function QuestionItem(){
+    return (
+        <div className="p-4 border border-gray-200 rounded-md hover:bg-gray-50 transition-colors">
+        <div className="flex justify-between">
+          <h3 className="font-medium text-gray-800 mb-1">Question 1</h3>
+        </div>
+        <p className="text-gray-700">What were the primary factors that led to your decision to leave the company?</p>
+      </div>
+    )
+}
