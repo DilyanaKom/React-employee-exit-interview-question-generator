6e8e9a13ac7637bef439a63f9c0812a77b662c25
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-09 14:02:15 +0300

    Complete question generator util

--- src/utils/questionGenerator.js
+++ src/utils/questionGenerator.js
@@ -1,6 +1,16 @@
 import allQuestions from '../../public/questions.json';
 
 export default function generateQuestions(selections){
-    console.log(allQuestions);
+    const {position, department, tenure, reason} = selections;
+
+    const filteredQuestions = allQuestions.filter(question =>
+        question.roles.includes(position) &&
+        question.departments.includes(department) &&
+        question.tenure.includes(tenure) &&
+        question.reasons.includes(reason)
+    )
+
+    const randomQuestions = [...filteredQuestions].sort(() => 0.5 - Math.random()).slice(0,4);
+    return randomQuestions;
 
 }
