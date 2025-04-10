90fe1d408004b36a4c84af7350c1077983c32105
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-10 13:56:44 +0300

    Update Questions.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,7 +1,7 @@
 import QuestionItem from "./QuestionItem";
 import { useNavigate, useLocation } from "react-router";
 
-import generateQuestions from "../utils/questionGenerator";
+import { useGenerateQuestions } from "../hooks/useGenerateQuestions";
 
 export default function Questions(){
 
@@ -9,8 +9,12 @@ export default function Questions(){
 
     const location = useLocation();
     const selections = location?.state;
+
+    const {questions, regenerate} = useGenerateQuestions(selections);
     
-    const questions = generateQuestions(selections);
+  const regenerateHandler = () => {
+    regenerate();
+  }
 
 
     return(
@@ -48,7 +52,9 @@ export default function Questions(){
           </svg>
           Back
         </button>
-        <button className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 flex items-center">
+        <button 
+        onClick={regenerateHandler}
+        className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 flex items-center">
           <svg className="h-4 w-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
           </svg>
