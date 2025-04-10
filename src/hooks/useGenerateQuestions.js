05e875c01504e391e225a20ebdcae0c4990b6228
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-10 13:56:38 +0300

    Add hook for question generation

---
+++ src/hooks/useGenerateQuestions.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { getRandomQuestions } from "../utils/randomQuestionGenerator";
+import { filterQuestions } from "../utils/questionFilter";
+
+export const useGenerateQuestions = (selections) => {
+    const [filtered, setFiltered] = useState([]);
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        const filteredQuestions = filterQuestions(selections);
+        setFiltered(filteredQuestions);
+        setQuestions(getRandomQuestions(filteredQuestions));
+    }, [selections]);
+
+    const regenerate = () => {
+        setQuestions(getRandomQuestions(filtered));
+    };
+
+    return {
+        questions,
+        regenerate
+    }
+}
