bcccdad67a17b5408380d5199fe9f4748ad17c3e
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-11 15:49:12 +0300

    Add copy text functionality

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -20,6 +20,12 @@ export default function Questions() {
     regenerate();
   }
 
+  const copyHandler = async () => {
+    const extractedData = questions.map(question => `${question.question}`).join('\n');
+    //const text = JSON.stringify(extractedData)
+    await navigator.clipboard.writeText(extractedData);
+  }
+
 
   return (
     <div
@@ -82,7 +88,9 @@ export default function Questions() {
               </svg>
               Export PDF
             </button>
-            <button className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 flex items-center">
+            <button 
+            onClick={copyHandler}
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 flex items-center">
               <svg className="h-4 w-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
               </svg>
@@ -94,3 +102,4 @@ export default function Questions() {
     </div>
   )
 }
+
